cf82c5907f07ac8b2778d0e9a025924f532e879b
Karel Miko <miko@dcit.cz>
2013-06-17 20:18:11 +0200

    version bump + CPANTS improvements

--- lib/CryptX.pm
+++ lib/CryptX.pm
@@ -3,7 +3,7 @@ package CryptX;
 use strict;
 use warnings ;
 
-our $VERSION = '0.011';
+our $VERSION = '0.012';
 $VERSION = eval $VERSION;
 
 require XSLoader;
@@ -62,6 +62,10 @@ L<Crypt::PRNG>, L<Crypt::PRNG::Fortuna>, L<Crypt::PRNG::Yarrow>, L<Crypt::PRNG::
 L<Crypt::KeyDerivation>
 
 =back
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 =head1 COPYRIGHT
 
