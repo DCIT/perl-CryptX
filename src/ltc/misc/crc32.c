8af0333c2bda7b005ceef3ab68e8f15d69b149c1
Karel Miko <karel.miko@gmail.com>
2016-05-09 22:15:28 +0200

    MSVC6 related fixes (needed for older ActivePerl@MSWin32)

--- src/ltc/misc/crc32.c
+++ src/ltc/misc/crc32.c
@@ -149,9 +149,10 @@ void crc32_init(crc32_state *ctx)
 
 void crc32_update(crc32_state *ctx, const unsigned char *input, unsigned long length)
 {
+   ulong32 crc;
    LTC_ARGCHKVD(ctx != NULL);
    LTC_ARGCHKVD(input != NULL);
-   ulong32 crc = ctx->crc;
+   crc = ctx->crc;
 
    while (length--)
       crc = crc32_m_tab[CRC32_INDEX(crc) ^ *input++] ^ CRC32_SHIFTED(crc);
@@ -161,13 +162,15 @@ void crc32_update(crc32_state *ctx, const unsigned char *input, unsigned long le
 
 void crc32_finish(crc32_state *ctx, void *hash, unsigned long size)
 {
+   unsigned char* h;
+   unsigned long i;
+   ulong32 crc;
+
    LTC_ARGCHKVD(ctx != NULL);
    LTC_ARGCHKVD(hash != NULL);
 
-   unsigned char* h = hash;
-   unsigned long i;
-
-   ulong32 crc = ctx->crc;
+   h = hash;
+   crc = ctx->crc;
    crc ^= _CRC32_NEGL;
    for (i = 0; i < size; i++) {
       h[size-i-1] = ((unsigned char*)&(crc))[i];
