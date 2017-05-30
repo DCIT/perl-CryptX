8af2bc897b09c7cb737e76c87a7ce474a54631df
Karel Miko <karel.miko@gmail.com>
2017-05-30 10:51:29 +0200

    ecc_dp_fill_from_sets fix

--- src/ltc/pk/ecc/ecc_dp_fill_from_sets.c
+++ src/ltc/pk/ecc/ecc_dp_fill_from_sets.c
@@ -12,6 +12,28 @@
 
 #ifdef LTC_MECC
 
+static int hexstrcmp(const char *hexa, const char *hexb)
+{
+  #define MY_TOLOWER(a) ((((a)>='A')&&((a)<='Z')) ? ((a)|0x60) : (a))
+  /* ignore leading zeroes */
+  while(*hexa == '0') hexa++;
+  while(*hexb == '0') hexb++;
+  /* compare: case insensitive, hexadecimal chars only */
+  while (*hexa && *hexb) {
+    if ( (*hexa < '0' || *hexa > '9') &&
+         (*hexa < 'a' || *hexa > 'f') &&
+         (*hexa < 'A' || *hexa > 'F') ) return 1;
+    if ( (*hexb < '0' || *hexb > '9') &&
+         (*hexb < 'a' || *hexb > 'f') &&
+         (*hexb < 'A' || *hexb > 'F') ) return 1;
+    if (MY_TOLOWER(*hexa) != MY_TOLOWER(*hexb)) return 1;
+    hexa++;
+    hexb++;
+  }
+  if (*hexa == '\0' && *hexb == '\0') return 0; /* success - match */
+  return 1;
+}
+
 /* search known curve by curve parameters and fill in missing parameters into dp
  * we assume every parameter has the same case (usually uppercase) and no leading zeros
  */
@@ -25,12 +47,12 @@ int ecc_dp_fill_from_sets(ltc_ecc_set_type *dp)
   if (!dp->prime || !dp->A || !dp->B || !dp->order || !dp->Gx || !dp->Gy || dp->cofactor == 0) return CRYPT_INVALID_ARG;
 
   for (x = 0; ltc_ecc_sets[x].size != 0; x++) {
-    if (XSTRCMP(ltc_ecc_sets[x].prime, dp->prime) == 0 &&
-        XSTRCMP(ltc_ecc_sets[x].A,     dp->A)     == 0 &&
-        XSTRCMP(ltc_ecc_sets[x].B,     dp->B)     == 0 &&
-        XSTRCMP(ltc_ecc_sets[x].order, dp->order) == 0 &&
-        XSTRCMP(ltc_ecc_sets[x].Gx,    dp->Gx)    == 0 &&
-        XSTRCMP(ltc_ecc_sets[x].Gy,    dp->Gy)    == 0 &&
+    if (hexstrcmp(ltc_ecc_sets[x].prime, dp->prime) == 0 &&
+        hexstrcmp(ltc_ecc_sets[x].A,     dp->A)     == 0 &&
+        hexstrcmp(ltc_ecc_sets[x].B,     dp->B)     == 0 &&
+        hexstrcmp(ltc_ecc_sets[x].order, dp->order) == 0 &&
+        hexstrcmp(ltc_ecc_sets[x].Gx,    dp->Gx)    == 0 &&
+        hexstrcmp(ltc_ecc_sets[x].Gy,    dp->Gy)    == 0 &&
         ltc_ecc_sets[x].cofactor == dp->cofactor) {
 
       params = ltc_ecc_sets[x];
