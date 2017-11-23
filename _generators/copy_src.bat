a6949d16891883090207ba373f2533f90d4ba813
Karel Miko <karel.miko@gmail.com>
2017-11-23 10:23:58 +0100

    cleanup _generators

--- _generators/copy_src.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo STARTED...
-xcopy /Y y:\_repos\libtommath\*.c d:\git\cryptx\src\ltm
-xcopy /Y y:\_repos\libtommath\*.h d:\git\cryptx\src\ltm
-xcopy /Y /E y:\_repos\libtomcrypt\src\* d:\git\cryptx\src\ltc
-perl fix_src.pl
-echo DONE!
-pause
--- _generators/gen.pl
+++ _generators/gen.pl
@@ -9,6 +9,7 @@ if ($ARGV[0] && $ARGV[0] =~ /genco/) {
   system($^X, qw[_generators/tt_mode.pl install_code]);
 }
 elsif ($ARGV[0] && $ARGV[0] =~ /gente/) {
+  die "blib dir not found, run 'make all'\n" unless -d 'blib';
   system($^X, qw[-Mblib _generators/tt_cipher.pl install_tests]);
   system($^X, qw[-Mblib _generators/tt_digest.pl install_tests]);
   system($^X, qw[-Mblib _generators/tt_mac.pl install_tests]);
