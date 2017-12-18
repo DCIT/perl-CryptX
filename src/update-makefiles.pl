3f24975dee982dc82e3021ab3b4db5c731805c3a
Karel Miko <karel.miko@gmail.com>
2017-12-18 19:49:04 +0100

    chmod fix

--- src/update-makefiles.pl
+++ src/update-makefiles.pl
@@ -4,13 +4,19 @@ use Modern::Perl;
 use File::Find qw(find);
 use File::Slurper qw(read_text write_text);
 use FindBin;
+
+warn "gonna chmod..\n";
+find({ wanted=>sub { system "chmod -x $_" if -f $_ && -x $_ && $_ =~ /\.(c|h)/ }, no_chdir=>1 }, "$FindBin::Bin/ltm", "$FindBin::Bin/ltc");
  
 my @objs = ();
+warn "gonna find ltc..\n";
 find({ wanted=>sub { push @objs, $_ if $_ =~ /\.c$/ && $_ !~ /tab\.c$/}, no_chdir=>1 }, "$FindBin::Bin/ltc");
+warn "gonna find ltm..\n";
 find({ wanted=>sub { push @objs, $_ if $_ =~ /\.c$/ }, no_chdir=>1 }, "$FindBin::Bin/ltm");
 
 my $o   = "OBJS=" . (join " ", map { s/\.c$/.o/r }   map { s!^.*/(ltm|ltc)/!$1/!r } @objs) =~ s/(.{80}) /$1 \\\n/gr;
 my $obj = "OBJS=" . (join " ", map { s/\.c$/.obj/r } map { s!^.*/(ltm|ltc)/!$1/!r } @objs) =~ s/(.{80}) /$1 \\\n/gr;
 
+warn "gonna write makefiles..\n";
 write_text("$FindBin::Bin/Makefile", read_text("$FindBin::Bin/Makefile") =~ s/OBJS=.+?\.o\n/$o\n/sr);
 write_text("$FindBin::Bin/Makefile.nmake", read_text("$FindBin::Bin/Makefile.nmake") =~ s/OBJS=.+?\.obj\n/$obj\n/sr);
