b9b5c69f91fb93d2027a127644776ba1f82f5a2c
Karel Miko <karel.miko@gmail.com>
2020-02-01 19:03:17 +0100

    appveyor test 15

--- .appveyor.cmd
+++ .appveyor.cmd
@@ -6,7 +6,7 @@ goto :eof
 cinst -y wget
 if "%perl_type%" == "cygwin" (
   start /wait c:\cygwin\setup-x86.exe -q -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel
-  set PATH=C:\cygwin\usr\local\bin;C:\cygwin\bin;%PATH%
+  set "PATH=C:\cygwin\usr\local\bin;C:\cygwin\bin;%PATH%"
   wget -q --no-check-certificate https://cpanmin.us/ -O C:\cygwin\tmp\downloaded-cpanm
   perl /tmp/downloaded-cpanm -v --notest App::cpanminus
   dir C:\cygwin\usr\local\bin
@@ -14,7 +14,7 @@ if "%perl_type%" == "cygwin" (
   rem -q --quiet-mode     Unattended setup mode
   rem -g --upgrade-also   also upgrade installed packages
   start /wait c:\cygwin\setup-x64.exe -q -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel
-  set PATH=C:\cygwin64\usr\local\bin;C:\cygwin64\bin;%PATH%
+  set "PATH=C:\cygwin64\usr\local\bin;C:\cygwin64\bin;%PATH%"
   wget -q --no-check-certificate https://cpanmin.us/ -O C:\cygwin64\tmp\downloaded-cpanm
   perl /tmp/downloaded-cpanm -v --notest App::cpanminus
 ) else if "%perl_type%" == "strawberry" (
