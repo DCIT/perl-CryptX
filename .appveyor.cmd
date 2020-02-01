9353284a520844571304c4591aa027092bb268a8
Karel Miko <karel.miko@gmail.com>
2020-02-01 17:53:09 +0100

    appveyor test 10

--- .appveyor.cmd
+++ .appveyor.cmd
@@ -11,7 +11,15 @@ if "%perl_type%" == "cygwin" (
   start /wait c:\cygwin\setup-x64.exe -q -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel
   set "PATH=C:\cygwin64\usr\local\bin;C:\cygwin64\bin;%PATH%"
 ) else if "%perl_type%" == "strawberry" (
+  cinst -y wget
   wget -q http://strawberryperl.com/download/%perl_version%/strawberry-perl-%perl_version%-%perl_bits%bit-portable.zip -O downloaded-strawberry.zip
+  7z x downloaded-strawberry.zip -oc:\spperl\
+  set "PATH=c:\spperl\perl\bin;c:\spperl\perl\site\bin;c:\spperl\c\bin;%PATH%"
+  wget -q --no-check-certificate https://cpanmin.us/ -O downloaded-cpanm
+  perl downloaded-cpanm --notest App::cpanminus
+) else if "%perl_type%" == "strawberry_old" (
+  cinst -y wget
+  wget -q http://strawberryperl.com/download/%perl_version%/strawberry-perl-%perl_version%-portable.zip -O downloaded-strawberry.zip
   7z x downloaded-strawberry.zip -oc:\spperl\
   set "PATH=c:\spperl\perl\bin;c:\spperl\perl\site\bin;c:\spperl\c\bin;%PATH%"
   wget -q --no-check-certificate https://cpanmin.us/ -O downloaded-cpanm
