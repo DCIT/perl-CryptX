f054247218af89d7d263852ff117fcc5c4ae9067
Karel Miko <karel.miko@gmail.com>
2020-02-01 17:41:14 +0100

    appveyor test 8

--- .appveyor.cmd
+++ .appveyor.cmd
@@ -12,6 +12,9 @@ dir c:\
 if "%perl_type%" == "cygwin" (
   start /wait c:\cygwin\setup-x86.exe -q -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel
   set "PATH=C:\cygwin\usr\local\bin;C:\cygwin\bin;%PATH%"
+) else if "%perl_type%" == "cygwin64" (
+  start /wait c:\cygwin\setup-x64.exe -q -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel
+  set "PATH=C:\cygwin64\usr\local\bin;C:\cygwin64\bin;%PATH%"
 ) else if "%perl_type%" == "strawberry" (
   wget -q http://strawberryperl.com/download/%perl_version%/strawberry-perl-%perl_version%-%perl_bits%bit-portable.zip -O downloaded-strawberry.zip
   7z x downloaded-strawberry.zip -oc:\spperl\
@@ -24,5 +27,9 @@ if "%perl_type%" == "cygwin" (
 )
 
 for /f "usebackq delims=" %%d in (`perl -MConfig -e"print $Config{make}"`) do set make=%%d
+
+if "%make%" == "gmake" (
+  make=gmake -j4
+)
 
 :eof
