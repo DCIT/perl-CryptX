abab5d85c1ea5f027bd03c4b63a4f948ebef43bf
Karel Miko <karel.miko@gmail.com>
2020-02-01 18:48:58 +0100

    appveyor test 11

--- .appveyor.cmd
+++ .appveyor.cmd
@@ -3,22 +3,24 @@ call :%*
 goto :eof
 
 :perl_setup
-
+cinst -y wget
 if "%perl_type%" == "cygwin" (
   start /wait c:\cygwin\setup-x86.exe -q -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel
   set "PATH=C:\cygwin\usr\local\bin;C:\cygwin\bin;%PATH%"
+  wget -q --no-check-certificate https://cpanmin.us/ -O C:\cygwin\tmp\downloaded-cpanm
+  perl /tmp/downloaded-cpanm --notest App::cpanminus
 ) else if "%perl_type%" == "cygwin64" (
   start /wait c:\cygwin\setup-x64.exe -q -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel
   set "PATH=C:\cygwin64\usr\local\bin;C:\cygwin64\bin;%PATH%"
+  wget -q --no-check-certificate https://cpanmin.us/ -O C:\cygwin64\tmp\downloaded-cpanm
+  perl /tmp/downloaded-cpanm --notest App::cpanminus
 ) else if "%perl_type%" == "strawberry" (
-  cinst -y wget
   wget -q http://strawberryperl.com/download/%perl_version%/strawberry-perl-%perl_version%-%perl_bits%bit-portable.zip -O downloaded-strawberry.zip
   7z x downloaded-strawberry.zip -oc:\spperl\
   set "PATH=c:\spperl\perl\bin;c:\spperl\perl\site\bin;c:\spperl\c\bin;%PATH%"
   wget -q --no-check-certificate https://cpanmin.us/ -O downloaded-cpanm
   perl downloaded-cpanm --notest App::cpanminus
 ) else if "%perl_type%" == "strawberry_old" (
-  cinst -y wget
   wget -q http://strawberryperl.com/download/%perl_version%/strawberry-perl-%perl_version%-portable.zip -O downloaded-strawberry.zip
   7z x downloaded-strawberry.zip -oc:\spperl\
   set "PATH=c:\spperl\perl\bin;c:\spperl\perl\site\bin;c:\spperl\c\bin;%PATH%"
