259e53ce9e41564a3e78107db94ddb825a88ede7
Karel Miko <karel.miko@gmail.com>
2024-09-03 20:07:42 +0200

    Copyright++

--- lib/CryptX.pm
+++ lib/CryptX.pm
@@ -111,6 +111,6 @@ This program is free software; you can redistribute it and/or modify it under th
 
 =head1 COPYRIGHT
 
-Copyright (c) 2013-2023 DCIT, a.s. L<https://www.dcit.cz> / Karel Miko
+Copyright (c) 2013-2024 DCIT, a.s. L<https://www.dcit.cz> / Karel Miko
 
 =cut
